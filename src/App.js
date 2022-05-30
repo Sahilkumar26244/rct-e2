faf353662bc0b1a653166e8771d7e77221b5cefd
Sahilkumar26244 <100782217+Sahilkumar26244@users.noreply.github.com>
2022-05-30 15:52:23 +0530

    evaluation

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AddProduct from "./components/AddProduct";
+import Pagination from "./components/Pagination";
+import Product from "./components/Product";
+import Products from "./components/Products"
+
+const App = () => {
+  return (
+  <div>
+   <AddProduct/>
+   <Pagination/>
+   <Product/>
+   <Products/>
+    sahil
+    </div>
+    )
+};
+
+export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AddProduct  from "./AddProduct";
+import Product from "./Product";
+import Pagination from "./Pagination";
+const Products = () => {
+  // TODO: Remove below const and instead import them from chakra
+  const Flex = () => <div />;
+  const Grid = () => <div />;
+
+  return (
+    <Flex>
+      {<AddProduct/>}
+      <Grid>{<Product/>}</Grid>
+      {<Pagination/>}
+    </Flex>
+  );
+};
+
+export default Products;
